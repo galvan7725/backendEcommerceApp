6412d9f0586b74b0b00dc52f138c7ce0f1fb9c9e
Luis Galvan <galvan7725@gmail.com>
2021-01-28 11:44:05 -0600

    Categories http methods

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,16 @@
 const {Category} = require('../models/Category');
 
+
+
+exports.allCategories = async(req, res)=>{
+    const categories = await Category.find();
+    if(categories.error || !categories ){
+        return res.status(404).send({error: 'categories not found'})
+    }else{
+        return res.status(200).send({status:'success',categories});
+    }
+}
+
 exports.categoryById = (req, res, next, id) =>{
     console.log(id);
     Category.findById(id).select('_id name icon color')
@@ -11,6 +22,18 @@ exports.categoryById = (req, res, next, id) =>{
             next();
         }
     })
+}
+
+exports.singleCategory =(req, res) =>{
+   try {
+    if(!req.category){
+        res.status(404).send({error:'not found'})
+    }else{
+        res.status(200).send(req.category);
+    }
+   } catch (error) {
+       res.status(500).send({error:'internal server error'});
+   }
 }
 
 exports.newCategory = async(req, res)=>{
@@ -27,6 +50,24 @@ exports.newCategory = async(req, res)=>{
         res.status(200).send(response);
     }
 
+}
+
+exports.updateCategory = async (req, res) => {
+    try {
+        const objectCategory = {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        }
+        const response = await Category.findByIdAndUpdate(req.category._id,objectCategory,{new: true});
+        if(response.error || !response){
+            res.status(500).send({error:'error updating category'});
+        }else{
+            res.status(200).send({status: 'success',response});
+        }
+    } catch (error) {
+        res.status(500).send({error: 'internal server error'});
+    }
 }
 
 exports.deleteCategory = async(req, res)=>{
--- routers/categories.js
+++ routers/categories.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { newCategory, deleteCategory, categoryById } = require('../controllers/categoryController');
+const { newCategory, deleteCategory, categoryById, singleCategory, allCategories, updateCategory } = require('../controllers/categoryController');
 
+
+router.get('/',allCategories);
 router.post('/',newCategory);
 router.delete('/:categoryId',deleteCategory);
+router.get('/:categoryId', singleCategory);
+router.put('/:categoryId', updateCategory);
 
 
 //params
